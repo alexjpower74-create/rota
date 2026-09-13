b918ab809d4cea992dd7c8a469f93ab93b606a3a
alexjpower74-create <alexjpower74@gmail.com>
2026-09-13 02:06:26 -0230

    cors: allow X-Editor

--- worker/src/util.js
+++ worker/src/util.js
@@ -18,7 +18,7 @@ export function json (data, status = 200, headers = {}) {
 export const CORS = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
-  'access-control-allow-headers': 'authorization,content-type',
+  'access-control-allow-headers': 'authorization,content-type,x-editor',
   'access-control-max-age': '86400'
 }
 
